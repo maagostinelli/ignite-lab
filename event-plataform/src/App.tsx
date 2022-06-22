b55c4100d4ee526725a04a06c00f5c304f2ec097
Mariana Agostinelli <marianaagost2@gmail.com>
2022-06-21 22:47:17 -0300

    initial commit

---
+++ event-plataform/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="text-2xl font-bold text-violet-600">Hello World</h1>
+  )
+}
+
+export default App
---
+++ event-plataform/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+    //qualquer arquivo dentro da pasta src de extensão tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
